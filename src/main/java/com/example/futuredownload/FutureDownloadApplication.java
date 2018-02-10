291cb03ba1089112431874425df1d95e055b542f
Michal Janczykowski <mjanczykowski@gmail.com>
2018-02-10 15:28:14 +0100

    init

---
+++ src/main/java/com/example/futuredownload/FutureDownloadApplication.java
@@ -0,0 +1,12 @@
+package com.example.futuredownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureDownloadApplication.class, args);
+	}
+}
